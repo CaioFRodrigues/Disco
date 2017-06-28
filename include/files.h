d6ccf113572352669a5b06bc6dd2f905f460c745
Ana Paula Mello <apcomello@gmail.com>
2017-06-28 19:05:10 -0300

    Add basic src files and clean repo from static files

---
+++ include/files.h
@@ -0,0 +1,13 @@
+/*
+*   UFRGS - Universidade Federal do Rio Grande do Sul
+*   Instituto de Informática
+*   INF01142 - Sistemas Operacionais I (2017/1)
+*
+*   Trabalho Prático II - Sistema de arquivos T2FS
+*
+*   Ana Paula Mello - 260723 <apcomello@gmail.com>
+*   Arateus Meneses - 242260 <arateus.meneses@gmail.com>
+*   Caio Rodrigues -261578  <caio_f.r@hotmail.com>
+*
+*   Versão: 16.2
+*/
---
+++ src/files.c
@@ -0,0 +1,17 @@
+/*
+*   UFRGS - Universidade Federal do Rio Grande do Sul
+*   Instituto de Informática
+*   INF01142 - Sistemas Operacionais I (2017/1)
+*
+*   Trabalho Prático II - Sistema de arquivos T2FS
+*
+*   Ana Paula Mello - 260723 <apcomello@gmail.com>
+*   Arateus Meneses - 242260 <arateus.meneses@gmail.com>
+*   Caio Rodrigues -261578  <caio_f.r@hotmail.com>
+*
+*   Versão: 16.2
+*/
+
+#include <stdio.h>
+#include "../include/apidisk.h"
+#include "../include/files.h"
