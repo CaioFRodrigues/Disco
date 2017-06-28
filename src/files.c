51c3ed8358cf0d039ee4830a366180f23418be9d
Ana Paula Mello <apcomello@gmail.com>
2017-06-28 20:08:51 -0300

    Add simple version of seek2

--- src/files.c
+++ src/files.c
@@ -14,4 +14,25 @@
 
 #include <stdio.h>
 #include "../include/apidisk.h"
+#include "../include/t2fs.h"
+#include "../include/bitmap2.h"
 #include "../include/files.h"
+
+// Mock array to represent the pointers to the open files
+int file_handles[20];
+t2fs_4tupla t2fs_directories[20];
+
+int seek2(FILE2 handle, DWORD offset){
+
+  if (offset != -1){
+    file_handles[handle] += offset;
+  }
+  else{
+    file_handles = t2fs_directories[handle].logicalBlockNumber + t2fs_directories[handle].numberOfContiguosBlocks + 1;
+  }
+
+  /* Se o valor de "offset" for "-1", o current_pointer deverá ser posicionado no byte seguinte ao final do arquivo,
+		Isso é útil para permitir que novos dados sejam adicionados no final de um arquivo já existente*/
+
+  return 0;
+}
