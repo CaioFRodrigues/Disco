4064c80623055b67c4d6be0d17d3d698696af6a7
Ana Paula Mello <apcomello@gmail.com>
2017-07-01 14:39:23 -0300

    Add simple logic flow for the delete2 (not functional)

--- src/files.c
+++ src/files.c
@@ -15,3 +15,31 @@
 #include <stdio.h>
 #include "../include/apidisk.h"
 #include "../include/files.h"
+
+int delete2(char *filename){
+
+  /*
+  *   Code to find the file_descriptor instance that belongs to the file name
+  *   Must return error code when a struct isn't found (i.e. invalid file name)
+  * return -1;
+  */
+  struct t2fs_4tupla file;  // Mock struct
+  iterateMFT(file);
+
+  return 0;
+}
+
+void iterateMFT(struct t2fs_4tupla file){
+  if (file.atributeType == 1){
+    file.atributeType = -1;
+    /*
+    *   Code to get the next MFT tuple structure
+    */
+    struct t2fs_4tupla next_tuple; // Mock struct
+    iterateMFT(next_tuple);
+  }
+  else if (file.atributeType == 0){ // Last MFT tuple for the file, end recursion
+    file.atributeType = -1;
+  }
+
+}
