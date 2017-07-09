fcde6bdecd247a2ef9a0760d3d5c149529608f53
Ana Paula Mello <apcomello@gmail.com>
2017-07-09 14:14:17 -0300

    Finish messy write2 function

--- src/files.c
+++ src/files.c
@@ -27,129 +27,126 @@
 // Ana
 int write2(FILE2 handle, char* buffer, int size){
 
-    unsigned char sector_buffer[SECTOR_SIZE]; // Buffer used to read from sector
-    unsigned char temp_buffer[SECTOR_SIZE]; // Buffer that will recieve the sliced buffer content
-    int current_byte = 0;
-    int current_sector_pointer = 0;
-    int first_byte = 3;
-    int i=0, j=0;
-    MFT* last_mft;
+  int first_byte = 2000;
 
-        // Update file register sizes (bytes and block)
-    // if (size+first_byte > opened_files[handle].fileSizeBytes){
-    //     int new_byte_size = size + first_byte;
-    //     int new_block_size = ceil(new_byte_size/1024.0);
+  int blocks_needed = ceil((size + first_byte)/1024.0);
+  int n = 0;
 
-    //     // Change file descriptor
-    //     opened_files[handle].current_pointer = new_byte_size + 1;
-    //     opened_files[handle].fileSizeBytes = new_byte_size;
+	MFT* mft = read_MFT(12);
 
-    //     // Change file register
-    // }
-    // else{
-    //     opened_files[handle].current_pointer = size+first_byte;
-    // }
+  /*
+    TODO:
+      Actually update the record
+    struct t2fs_record directory_record;    This is gonna be found by another function
+    directory_record.blocksFileSize = blocks_needed;
+    if (size > directory_record.bytesFileSize)
+      directory_record.bytesFileSize = size;
 
-    MFT* mft = read_MFT(12);  // For testing only
+    write record to disk
 
-    // Writes buffer to file
-    while (size > 0){
-        while (mft != NULL){    // Must be changed to mft-> next !=NULL when mft stores the tuple with attributeType = 0
-            int first_block = mft->current_MFT.logicalBlockNumber;
-            int number_blocks = mft->current_MFT.numberOfContiguosBlocks;
+  */
+  MFT *last_mft = mft;
 
-            int num_sectors = number_blocks * 4;
-            int first_sector = first_block * 4;
-
-            // Loops through contiguos sectors from MFT
-            for (j=0; j<num_sectors; j++){
-
-                current_sector_pointer = 0;
-
-                int sector_current = ceil(ceil(first_byte/256.0)/4.0)-1;
-
-                if (sector_current == i){
-                    current_sector_pointer = first_byte;
-                }
-
-                if (size > (256-current_sector_pointer)){   // Fills entire sector
-
-                    for (i=0; i<(256-current_sector_pointer); i++){
-                        temp_buffer[i] = buffer[current_byte];
-                        current_byte++;
-                    }
-                    size -= (256-current_sector_pointer);
-                    insert_in_sector(j+first_sector, temp_buffer, current_sector_pointer, (256-current_sector_pointer));
-                }
-                else{   // Writes last bytes into sector
-                    for (i=0; i<size; i++){
-                        temp_buffer[i] = buffer[current_byte];
-                        current_byte++;
-                    }
-                    insert_in_sector(j+first_sector, temp_buffer, current_sector_pointer, size);
-                    return 0;
-                }
-            }
-            last_mft = mft; // Stores previous mft tuple for when the loop ends
-            mft = mft->next;
-        }
+  while (last_mft->next != NULL){
+    last_mft =last_mft->next;
+  }
 
 
-        /*
-        *   BASIC LOGIC FOR THIS PART
-        *       Allocates new block
-        *           Checks if the block is contiguos with the blocks from the last mft tuple from the file
-        *               If it is, just update teh mft tuple
-        *           If it isn't, create new tuple and write it to block
-        *
-        *
-        */
+  int block_size = 3;
 
+  // if (blocks_needed > ceil(opened_files[handle].bytesFileSize)/1024.0)
+  if (blocks_needed > block_size){
+    // blocks_needed -= ceil(opened_files[handle].bytesFileSize)/1024.0;
+    blocks_needed -= block_size;
 
-        int blocks_needed = ceil(size/1024.0);
-        int n = 0;
-        int new_block = 0;
+    for (n=0; n<blocks_needed;n++){
 
-        for (n=0; n < blocks_needed; n++){
+      int new_block = searchBitmap2(0);
+      if (setBitmap2(new_block, 1) != 0){
+        return -1;
+      }
 
-            new_block = searchBitmap2(0);
+      // // New block is contiguos to the last one
+      if (new_block == last_mft->current_MFT.logicalBlockNumber + last_mft->current_MFT.numberOfContiguosBlocks){
+        last_mft->current_MFT.numberOfContiguosBlocks++;
+        write_first_tuple_MFT_and_set_0_second(last_mft->sector, (last_mft->offset)*16, last_mft->current_MFT);
+      }
+      else{ // New block needs new tuple
+        struct t2fs_4tupla new_tuple;
+        new_tuple.atributeType = 1;
+        new_tuple.logicalBlockNumber = new_block;
+        new_tuple.virtualBlockNumber = last_mft->current_MFT.virtualBlockNumber + last_mft->current_MFT.numberOfContiguosBlocks;
+        new_tuple.numberOfContiguosBlocks = 1;
 
-            if (new_block < 0){
+        // Treat situation in which sector and/or register is full
+        write_first_tuple_MFT_and_set_0_second(last_mft->sector, (last_mft->offset+1)*16, new_tuple);
+        mft = push_MFT(mft, new_tuple, last_mft->sector, (last_mft->offset)+1);
+        last_mft = last_mft->next;
 
-                return -1;
-            }
-
-            if (setBitmap2(new_block, 1) != 0){
-                return -1;
-            }
-        }
-
-        if (new_block == last_mft->current_MFT.logicalBlockNumber + last_mft->current_MFT.numberOfContiguosBlocks + 1){
-            // Block contiguos to the ones in the last MFT
-            last_mft->current_MFT.numberOfContiguosBlocks++;
-        }
-        else{
-            last_mft->current_MFT.atributeType = -1;    // Invalidates what used to be last tuple for file (attributeType = 0)
-
-            // Create new MFT tuple
-            struct t2fs_4tupla new_tuple;
-            new_tuple.atributeType = 2;
-            new_tuple.virtualBlockNumber = -1;  // Needs to point to the next one, probably updated later
-            new_tuple.logicalBlockNumber = new_block;
-            new_tuple.numberOfContiguosBlocks = 1;
-
-            // mft->current_MFT.virtualBlockNumber = Needs to point to the new_tuple logical block
-            if (last_mft->offset == 15){
-                // Needs new sector
-                mft = push_MFT(mft, new_tuple, last_mft->sector+1, 0);
-            }
-            else{
-                mft = push_MFT(mft, new_tuple, last_mft->sector, last_mft->offset+1);
-            }
-            last_mft = mft;
-                
-        }
+      }
     }
+  }
+
+  unsigned char sector_buffer[SECTOR_SIZE]; // Buffer used to read from sector
+  unsigned char temp_buffer[SECTOR_SIZE]; // Buffer that will recieve the sliced buffer content
+
+  int current_byte = 0;
+  int current_sector_pointer = 0;
+  int i=0, j=0;
+  MFT* all_mft_list = read_MFT(12);
+
+  int pointer_block = first_byte/1024; // Virtual block in which first_byte is in
+  int pointer_sector = first_byte/256; //Logical sector in which first byte is in
+
+	while (all_mft_list != NULL){    // Must be changed to mft-> next !=NULL when mft stores the tuple with attributeType = 0
+		
+    int passed_blocks = all_mft_list->current_MFT.virtualBlockNumber;
+    int passed_sectors = 4 * passed_blocks;
+    if (pointer_block <= passed_blocks){
+
+      int number_blocks = all_mft_list->current_MFT.numberOfContiguosBlocks;
+      int first_block = all_mft_list->current_MFT.logicalBlockNumber;
+
+      int num_sectors = number_blocks * 4;
+      int first_sector = first_block * 4;        
+
+      for (j=0; j<num_sectors; j++){
+        current_sector_pointer = 0;
+        if (pointer_sector <= j+passed_sectors){
+
+          if (pointer_sector == j+passed_sectors){
+            current_sector_pointer = first_byte - (256*pointer_sector);
+          }
+
+          if (size > (256-current_sector_pointer)){   // Fills entire sector
+
+            for (i=0; i<(256-current_sector_pointer); i++){
+              temp_buffer[i] = buffer[current_byte];
+              current_byte++;
+            }
+            size -= (256-current_sector_pointer);
+            insert_in_sector(j+first_sector, temp_buffer, current_sector_pointer, (256-current_sector_pointer));
+          }
+          else{   // Writes last bytes into sector
+            for (i=0; i<size; i++){
+              temp_buffer[i] = buffer[current_byte];
+              current_byte++;
+            }
+
+            insert_in_sector(j+first_sector, temp_buffer, current_sector_pointer, size);
+            return 0;
+          }
+
+        }
+
+      }
+
+    }
+
+
+    all_mft_list = all_mft_list->next;
+	}
+
 }
 
 // Ana
